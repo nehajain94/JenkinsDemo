4e48f6033e97f50c6627ed4a7acbcb8918613036
nehajain94 <39215523+nehajain94@users.noreply.github.com>
2022-03-08 15:12:52 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+import os
+
+list1 = [8,9,3,6,1,10]
+list1.reverse()
+print("The reversed list is", list1)
+
+list2 = [92, 67, 120, 34, 76, 54, 78,87, 65, 56, 345]
+list2.sort()
+print("The sorted list is ", list2)
+
+# list3 = []
+list3 = list1.copy()
+print("list 3 ", list3)
+
+index_value = list2[2:6]
+print("the index values are", index_value)
+
